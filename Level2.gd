7ed65861498df1a5b2570f5fa18967617d340796
dptheodore <dptheodore93@gmail.com>
2019-12-21 22:36:44 -0500

    addded sfx lib

--- Level2.gd
+++ Level2.gd
@@ -1,1 +1,25 @@
 extends Node
+
+func _ready():
+	$Player.connect("hitPinata",self,"onPinataHit")
+	$Player.connect("hitWorker",self, "playGrunt")
+	$Player.connect("hitChair",self, "playChair")
+	$Player.connect("hitWall",self, "playBump")
+	$Player.connect("collide", self, "playBump")
+	
+	
+func onPinataHit():
+	playYay()
+	Global.goto_scene("res://Level2.tscn")
+
+func playYay():
+	$sfx.play("yay")
+
+func playGrunt():
+	$sfx.play("grunt")
+
+func playChair():
+	$sfx.play("chair_spin")
+	
+func playBump():
+	$sfx.play("bump")
