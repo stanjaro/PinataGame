914690d7b2f849639219c07f4223a9bd991d2351
unknown <stanjarozews@aol.com>
2019-12-21 20:50:56 -0500

    cooldowns set up for oof and hit sounds

--- Player.gd
+++ Player.gd
@@ -2,8 +2,15 @@ extends KinematicBody
 
 export (NodePath) var camera
 
+var SFX_COOLDOWN = 3
+
 var max_speed = 8
 var mouse_sensitivity = 0.002
+var hit_cooldown = SFX_COOLDOWN
+var collide_cooldown = SFX_COOLDOWN/3
+var hit_pressed = false
+var collide_should_sound = true
+var sfx_on = false
 
 var current_collide = "none"
 signal collidePinata()
@@ -20,21 +27,48 @@ func get_input():
 		input_dir += -get_global_transform().basis.x
 	if Input.is_action_pressed("ui_right"):
 		input_dir += get_global_transform().basis.x
+	if Input.is_action_pressed("ui_select"):
+		check_hit()
 	input_dir = input_dir.normalized()
 	return input_dir
 
 func set_current_collide(name):
+	
 	if name.begins_with("Chair"):
 		current_collide = "chair"
 	elif name.begins_with("Worker"):
 		current_collide = "worker"
 	elif name.begins_with("Pinata"):
 		current_collide = "pinata"
-		emit_signal("collidePinata")
 	elif name.begins_with("Wall"):
 		current_collide = "wall"
 	else:
 		current_collide = "none"
+
+func check_hit():
+	#print("you hit" + current_collide)
+	if current_collide == "chair" and !hit_pressed and sfx_on == false:
+		print("SPIN")
+		hit_pressed = true
+		sfx_on = true
+		emit_signal("hitChairr")
+	elif current_collide == "worker" and !hit_pressed and sfx_on == false:
+		print("OVER THERE")
+		hit_pressed = true
+		sfx_on = true
+		emit_signal("hitWorker")
+	elif current_collide == "pinata" and !hit_pressed and sfx_on == false:
+		print("YAY")
+		emit_signal("collidePinata")
+		hit_pressed = true
+		sfx_on = true
+	elif current_collide == "wall" and !hit_pressed and sfx_on == false:
+		print("OOF")
+		hit_pressed = true
+		sfx_on = true
+		emit_signal("hitWall")
+	else:
+		return
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
@@ -51,8 +85,25 @@ func _physics_process(delta):
 	var collision_info = get_slide_collision(0)
 	if collision_info:
 		set_current_collide(collision_info.collider.name)
-		print(current_collide)
+		if collide_should_sound and sfx_on == false:
+			print("collide")
+			collide_should_sound = false
+			sfx_on = true
+			emit_signal("collide")
 	else:
 		set_current_collide("none")
-
+	if hit_cooldown > 0 and hit_pressed and sfx_on == true:
+		hit_cooldown -= delta
+		#print(cooldown)
+	if hit_cooldown < 0:
+		hit_cooldown = SFX_COOLDOWN
+		hit_pressed = false
+		sfx_on = false
+	if collide_cooldown > 0 and !collide_should_sound and sfx_on == true:
+		collide_cooldown -= delta
+		#print(collide_cooldown)
+	if collide_cooldown <= 0:
+		collide_cooldown = SFX_COOLDOWN/3
+		collide_should_sound = true
+		sfx_on = false
 
